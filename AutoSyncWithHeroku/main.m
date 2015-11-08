b2a58dbd35ea33bd3ceabe3b0b285a6440044002
GitGitChen <ChenJFChn@163.com>
2015-11-09 00:28:23 +0800

    nihao

--- AutoSyncWithHeroku/main.m
+++ AutoSyncWithHeroku/main.m
@@ -13,6 +13,7 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+         //nihao
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([aswhAppDelegate class]));
     }
 }
